9f8c03c95fd18b5bab2b667acd1c27c3ffa393c0
jasung <jasung@users.noreply.github.com>
2026-05-24 22:30:43 +0900

    빌드번호_6_업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.codex.indown"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "0.3.0"
     }
 
